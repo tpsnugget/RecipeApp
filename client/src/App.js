34a9f7632d1e0eb1a143b1603c42a1b5895d2116
Michael Giebner <mikegiebner@gmail.com>
2019-12-05 16:40:26 -0500

    client front-end is now connected to the api back-end. No mongoose yet.

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,21 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+
+  const [apiResponse, setApiResponse] = useState("")
+
+  useEffect( () => callAPI() )
+
+  function callAPI(){
+    fetch("http://localhost:9000/recipes")
+      .then(res => res.text())
+      .then(res => setApiResponse(res))
+  }
+
   return (
     <div className="App">
-      <h1>Client is up man!</h1>
+      <h1>{apiResponse}</h1>
     </div>
   );
 }
