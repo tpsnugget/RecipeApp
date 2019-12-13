594dced04bb70c04cc0c5a1f86b6cc82afe743ac
Michael Giebner <mikegiebner@gmail.com>
2019-12-13 09:04:50 -0500

    Lots of Add a New Recipe css, added cancel button, moved some stuff out to AddIngredients.js. Will move some later to AddPrep.js.

---
+++ client/src/AddIngredients.js
@@ -0,0 +1,69 @@
+<p>Ingredients:</p>
+   <label>Amount
+                           <input
+         className="AddRecipe-input"
+         type="text"
+         placeholder="Ingredient"
+         name="ingredients"
+         onChange={this.handleChange}>
+      </input>
+   </label>
+   <label>Ingredient
+                           <input
+         className="AddRecipe-input"
+         type="text"
+         placeholder="Ingredient"
+         name="ingredients"
+         onChange={this.handleChange}>
+      </input>
+   </label>
+
+   <label>Prep:
+                           <input
+         className="AddRecipe-input"
+         type="text"
+         placeholder="Prep"
+         name="prep"
+         onChange={this.handleChange}>
+      </input>
+   </label>
+
+   <label>Cooked:
+                           <input
+         className="AddRecipe-input"
+         type="text"
+         placeholder="Yes or No"
+         name="cooked"
+         onChange={this.handleChange}>
+      </input>
+   </label>
+
+   <label>Cooked Date:
+                           <input
+         className="AddRecipe-input"
+         type="text"
+         placeholder="Cooked Date"
+         name="cooked_date"
+         onChange={this.handleChange}>
+      </input>
+   </label>
+
+   <label>Keywords:
+                           <input
+         className="AddRecipe-input"
+         type="text"
+         placeholder="Keywords"
+         name="keywords"
+         onChange={this.handleChange}>
+      </input>
+   </label>
+
+   <label>Rating:
+                           <input
+         className="AddRecipe-input"
+         type="text"
+         placeholder="Rating"
+         name="rating"
+         onChange={this.handleChange}>
+      </input>
+   </label>
